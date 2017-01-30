29b9268df72ce612e06aaa44409ad9576c6fadd7
Xiaodong <yxd59493942@gmail.com>
2017-01-30 16:09:06 +0100

    draft strategy

--- examples/my_strategy.py
+++ examples/my_strategy.py
@@ -3,6 +3,7 @@ from soccersimulator.mdpsoccer import SoccerTeam, Simulation, SoccerAction
 from soccersimulator.gui import SimuGUI,show_state,show_simu
 from soccersimulator.utils import Vector2D
 
+from soccersimulator import settings
 
 
 ## Strategie aleatoire
@@ -12,10 +13,30 @@ class RandomStrategy(Strategy):
     def compute_strategy(self,state,id_team,id_player):
         return SoccerAction(Vector2D.create_random(),Vector2D.create_random())
 
+
+## Strategie aleatoire
+class StraightStrategy(Strategy):
+    def __init__(self):
+        Strategy.__init__(self,"Random")
+    def compute_strategy(self,state,id_team,id_player):
+    	ball = state.ball
+    	pos_ball = ball.position
+    	state_player = state.player_state(id_team, id_player)
+    	pos_player = state_player.position
+    	acc = (pos_ball - pos_player).normalize()
+    	if pos_player.distance(pos_ball) <= (settings.PLAYER_RADIUS + settings.BALL_RADIUS):
+    		shoot = acc
+    		acc = Vector2D()
+    	else: 
+    		shoot = None
+    	print ball.vitesse
+        return SoccerAction(acc,shoot)
+
+
 ## Creation d'une equipe
 team1 = SoccerTeam(name="team1",login="etu1")
 team2 = SoccerTeam(name="team2",login="etu2")
-team1.add("John",Strategy()) #Strategie qui ne fait rien
+team1.add("John",StraightStrategy()) 
 team2.add("Paul",RandomStrategy())   #Strategie aleatoire
 
 #Creation d'une partie
